a00baaa8427a27718ad2d476c3c2ab5f061681a0
Gülfem Gözalıcı <162334353+gulfemgozalici@users.noreply.github.com>
2026-02-01 01:33:57 +0300

    Add files via upload
    Basit hesap makinesi.

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn3 = New Button()
+        btn2 = New Button()
+        btn1 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnClear = New Button()
+        btn0 = New Button()
+        btnEsittir = New Button()
+        lblSonuc = New Label()
+        btnToplama = New Button()
+        btnCarpma = New Button()
+        btnBolme = New Button()
+        btnCikarma = New Button()
+        txtSonuc = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btn3
+        ' 
+        btn3.Location = New Point(227, 109)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(104, 51)
+        btn3.TabIndex = 0
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Location = New Point(117, 109)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(104, 51)
+        btn2.TabIndex = 1
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn1
+        ' 
+        btn1.Location = New Point(7, 109)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(104, 51)
+        btn1.TabIndex = 2
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Location = New Point(7, 166)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(104, 51)
+        btn4.TabIndex = 5
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Location = New Point(117, 166)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(104, 51)
+        btn5.TabIndex = 4
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Location = New Point(227, 166)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(104, 51)
+        btn6.TabIndex = 3
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Location = New Point(7, 223)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(104, 51)
+        btn7.TabIndex = 8
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Location = New Point(117, 223)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(104, 51)
+        btn8.TabIndex = 7
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Location = New Point(227, 223)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(104, 51)
+        btn9.TabIndex = 6
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        btnClear.Location = New Point(7, 280)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(104, 51)
+        btnClear.TabIndex = 11
+        btnClear.Text = "C"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Location = New Point(117, 280)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(104, 51)
+        btn0.TabIndex = 10
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnEsittir
+        ' 
+        btnEsittir.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        btnEsittir.Location = New Point(227, 280)
+        btnEsittir.Name = "btnEsittir"
+        btnEsittir.Size = New Size(104, 51)
+        btnEsittir.TabIndex = 9
+        btnEsittir.Text = "="
+        btnEsittir.UseVisualStyleBackColor = True
+        ' 
+        ' lblSonuc
+        ' 
+        lblSonuc.AutoSize = True
+        lblSonuc.Location = New Point(12, 38)
+        lblSonuc.Name = "lblSonuc"
+        lblSonuc.Size = New Size(43, 15)
+        lblSonuc.TabIndex = 12
+        lblSonuc.Text = "Sonuç:"
+        ' 
+        ' btnToplama
+        ' 
+        btnToplama.Location = New Point(117, 9)
+        btnToplama.Name = "btnToplama"
+        btnToplama.Size = New Size(104, 44)
+        btnToplama.TabIndex = 13
+        btnToplama.Text = "+"
+        btnToplama.UseVisualStyleBackColor = True
+        ' 
+        ' btnCarpma
+        ' 
+        btnCarpma.Location = New Point(117, 59)
+        btnCarpma.Name = "btnCarpma"
+        btnCarpma.Size = New Size(104, 44)
+        btnCarpma.TabIndex = 14
+        btnCarpma.Text = "*"
+        btnCarpma.UseVisualStyleBackColor = True
+        ' 
+        ' btnBolme
+        ' 
+        btnBolme.Location = New Point(227, 62)
+        btnBolme.Name = "btnBolme"
+        btnBolme.Size = New Size(104, 44)
+        btnBolme.TabIndex = 16
+        btnBolme.Text = "/"
+        btnBolme.UseVisualStyleBackColor = True
+        ' 
+        ' btnCikarma
+        ' 
+        btnCikarma.Location = New Point(227, 12)
+        btnCikarma.Name = "btnCikarma"
+        btnCikarma.Size = New Size(104, 44)
+        btnCikarma.TabIndex = 15
+        btnCikarma.Text = "-"
+        btnCikarma.UseVisualStyleBackColor = True
+        ' 
+        ' txtSonuc
+        ' 
+        txtSonuc.Location = New Point(12, 71)
+        txtSonuc.Name = "txtSonuc"
+        txtSonuc.Size = New Size(99, 23)
+        txtSonuc.TabIndex = 17
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(343, 346)
+        Controls.Add(txtSonuc)
+        Controls.Add(btnBolme)
+        Controls.Add(btnCikarma)
+        Controls.Add(btnCarpma)
+        Controls.Add(btnToplama)
+        Controls.Add(lblSonuc)
+        Controls.Add(btnClear)
+        Controls.Add(btn0)
+        Controls.Add(btnEsittir)
+        Controls.Add(btn7)
+        Controls.Add(btn8)
+        Controls.Add(btn9)
+        Controls.Add(btn4)
+        Controls.Add(btn5)
+        Controls.Add(btn6)
+        Controls.Add(btn1)
+        Controls.Add(btn2)
+        Controls.Add(btn3)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnEsittir As Button
+    Friend WithEvents lblSonuc As Label
+    Friend WithEvents btnToplama As Button
+    Friend WithEvents btnCarpma As Button
+    Friend WithEvents btnBolme As Button
+    Friend WithEvents btnCikarma As Button
+    Friend WithEvents txtSonuc As TextBox
+
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private currentValue As Double = 0
+    Private previousValue As Double = 0
+    Private operation As String = ""
+    Private isNewEntry As Boolean = True
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Form Başlığı
+        Me.Text = "Hesap Makinesi"
+        txtSonuc.Text = "0"
+    End Sub
+    Private Sub btnSayi_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+        Dim btn As Button = CType(sender, Button)
+        If isNewEntry Then
+            txtSonuc.Text = btn.Text
+            isNewEntry = False
+        Else
+            If txtSonuc.Text = "0" Then
+                txtSonuc.Text = btn.Text
+            Else
+                txtSonuc.Text &= btn.Text
+            End If
+        End If
+    End Sub
+    ' İşlem butonları - Tüm işlemler için tek event
+    Private Sub OperationButton_Click(sender As Object, e As EventArgs) Handles btnToplama.Click, btnCikarma.Click, btnCarpma.Click, btnBolme.Click
+        Dim btn As Button = CType(sender, Button)
+
+        previousValue = Double.Parse(txtSonuc.Text)
+        operation = btn.Text
+        isNewEntry = True
+    End Sub
+
+    ' Eşittir butonu - Hesaplama yapar
+    Private Sub btnEsittir_Click(sender As Object, e As EventArgs) Handles btnEsittir.Click
+        If operation = "" Then Return
+
+        currentValue = Double.Parse(txtSonuc.Text)
+        Dim result As Double = 0
+
+        Select Case operation
+            Case "+"
+                result = previousValue + currentValue
+            Case "-"
+                result = previousValue - currentValue
+            Case "*"
+                result = previousValue * currentValue
+            Case "/"
+                If currentValue <> 0 Then
+                    result = previousValue / currentValue
+                Else
+                    txtSonuc.Text = "Hata!"
+                    MessageBox.Show("Sıfıra bölme hatası!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Return
+                End If
+        End Select
+
+        txtSonuc.Text = result.ToString()
+        isNewEntry = True
+        operation = ""
+    End Sub
+
+    ' Temizle butonu - Hesap makinesini sıfırlar
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtSonuc.Text = "0"
+        currentValue = 0
+        previousValue = 0
+        operation = ""
+        isNewEntry = True
+    End Sub
+End Class
